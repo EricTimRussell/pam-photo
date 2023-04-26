0642d6bad8c18a34ce2afd731d0e2b4f372fe84e
Eric Russell <Erictim.Russell@live.com>
2023-04-26 08:15:24 -0600

    featured img masonry

---
+++ src/components/FeaturedImagesComponent.vue
@@ -0,0 +1,68 @@
+<template>
+  <!-- Featured Images -->
+  <section class="container-fluid mb-5">
+    <div class="row">
+      <div class="col-12 d-flex justify-content-center pt-5">
+        <h2>Featured Photo's</h2>
+      </div>
+      <div class="col-12 d-flex justify-content-center">
+        <span class="section-divider"></span>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <!-- photo row 1 -->
+      <div class="col-12 col-md-2 d-flex justify-content-center pt-2 small-pad-x">
+        <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
+      </div>
+      <div class="col-12 col-md-4 d-flex justify-content-center pt-2 small-pad-x">
+        <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
+      </div>
+      <div class="col-12 col-md-4 d-flex justify-content-center pt-2 small-pad-x">
+        <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
+      </div>
+    </div>
+    <!-- photo row 2 -->
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-5 d-flex justify-content-center pt-2 small-pad-x">
+        <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
+      </div>
+      <div class="col-12 col-md-2 d-flex justify-content-center pt-2 small-pad-x">
+        <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
+      </div>
+      <div class="col-12 col-md-3 d-flex justify-content-center pt-2 small-pad-x">
+        <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
+      </div>
+    </div>
+    <!-- photo row 3 -->
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-2 d-flex justify-content-center pt-2 small-pad-x">
+        <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
+      </div>
+      <div class="col-12 col-md-6 d-flex justify-content-center pt-2 small-pad-x">
+        <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
+      </div>
+      <div class="col-12 col-md-2 d-flex justify-content-center pt-2 small-pad-x">
+        <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  setup() {
+
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.featured-img {
+  object-fit: cover;
+  display: block;
+  width: 100%;
+  height: 35vh;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,55 +28,13 @@
   </section>
 
   <!-- Featured Images -->
-  <section class="container-fluid mb-5">
-    <div class="row">
-      <div class="col-12 d-flex justify-content-center pt-5">
-        <h2>Featured Photo's</h2>
-      </div>
-      <div class="col-12 d-flex justify-content-center">
-        <span class="section-divider"></span>
-      </div>
-    </div>
-    <div class="row justify-content-center">
-      <!-- photo row 1 -->
-      <div class="col-12 col-md-2 d-flex justify-content-center gap-5 pt-5">
-        <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
-      </div>
-      <div class="col-12 col-md-2 d-flex justify-content-center gap-5 pt-5">
-        <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
-      </div>
-      <div class="col-12 col-md-2 d-flex justify-content-center gap-5 pt-5">
-        <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
-      </div>
-      <!-- photo row 2 -->
-      <div class="row justify-content-center">
-        <div class="col-12 col-md-2 d-flex justify-content-center gap-5 pt-5">
-          <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
-        </div>
-        <div class="col-12 col-md-2 d-flex justify-content-center gap-5 pt-5">
-          <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
-        </div>
-        <div class="col-12 col-md-2 d-flex justify-content-center gap-5 pt-5">
-          <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
-        </div>
-      </div>
-      <!-- photo row 3 -->
-      <div class="row justify-content-center">
-        <div class="col-12 col-md-2 d-flex justify-content-center gap-5 pt-5">
-          <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
-        </div>
-        <div class="col-12 col-md-2 d-flex justify-content-center gap-5 pt-5">
-          <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
-        </div>
-        <div class="col-12 col-md-2 d-flex justify-content-center gap-5 pt-5">
-          <img class="featured-img elevation" src="../assets/placeholder.jpg" alt="">
-        </div>
-      </div>
-    </div>
-  </section>
+  <div>
+    <FeaturedImagesComponent />
+  </div>
 </template>
 
 <script>
+import FeaturedImagesComponent from "../components/FeaturedImagesComponent.vue";
 import HeroComponent from "../components/HeroComponent.vue";
 import { appState } from "../stores/AppState"
 import { computed } from "@vue/reactivity"
@@ -86,7 +44,7 @@ export default {
       carousel: computed(() => appState.carousel),
     };
   },
-  components: { HeroComponent }
+  components: { HeroComponent, FeaturedImagesComponent }
 }
 </script>
 
@@ -95,56 +53,5 @@ export default {
   background-color: gray;
   width: 100px;
   height: 5px;
-}
-
-.carousel-img {
-  object-fit: cover;
-  display: block;
-  width: auto;
-  min-width: 100%;
-  height: 55vh;
-}
-
-.featured-img {
-  object-fit: cover;
-  display: block;
-  width: 100%;
-  height: 35vh;
-  max-width: fit-content;
-}
-
-.carousel-container {
-  margin-left: 20%;
-  margin-right: 20%;
-}
-
-.ml {
-  margin-left: 20%;
-}
-
-.mr {
-  margin-right: 20%;
-}
-
-@media only screen and (max-width: 770px) {
-  .carousel-img {
-    object-fit: cover;
-    display: block;
-    width: auto;
-    height: 25vh;
-  }
-
-  .ml {
-    margin-left: 15%;
-  }
-
-  .mr {
-    margin-right: 15%;
-  }
-
-  .carousel-container {
-    margin-left: 15%;
-    margin-right: 15%;
-  }
 }
 </style>
