ce18935307645c718801b1712391bd0ce2fee78c
Eric Russell <Erictim.Russell@live.com>
2023-05-10 09:41:16 -0600

    featured images mobile

--- src/components/FeaturedImagesComponent.vue
+++ src/components/FeaturedImagesComponent.vue
@@ -131,11 +131,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.featured-img {
-  object-fit: cover;
-  display: block;
-  width: 100%;
-  height: 35vh;
-}
-</style>
+<style lang="scss" scoped></style>
