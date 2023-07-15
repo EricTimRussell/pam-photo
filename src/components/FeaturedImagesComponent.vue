fe964fef69f771e3403e4efa30d260510e9105cb
Eric Russell <Erictim.Russell@live.com>
2023-07-14 20:43:28 -0600

    featured img border color change

--- src/components/FeaturedImagesComponent.vue
+++ src/components/FeaturedImagesComponent.vue
@@ -38,13 +38,13 @@
     </div>
     <!-- photo row 3 -->
     <div class="row justify-content-center">
-      <div class="col-1 bg-dark mt-2">
+      <div class="col-1 footer-bg mt-2">
       </div>
       <div class="col-12 col-md-8 d-flex justify-content-center pt-2 small-pad-x" v-motion-slide-visible-once-bottom>
         <img oncontextmenu="return false" ondragstart="return false" class="featured-img elevation selectable"
           :src="imageUrl8" alt="" data-bs-toggle="modal" data-bs-target="#featured8">
       </div>
-      <div class="col-1 bg-dark mt-2">
+      <div class="col-1 footer-bg mt-2">
       </div>
     </div>
     <!-- photo row 4 -->
