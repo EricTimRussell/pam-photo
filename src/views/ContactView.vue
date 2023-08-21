6e4835bba1f025b2e5ee2c40432434126eea9dc9
Eric Russell <Erictim.Russell@live.com>
2023-08-21 11:48:54 -0600

    small redesign of contact page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="container-fluid view-height fade-in">
-    <div class="row justify-content-center my-3 no-mobile">
-      <div class="col-10">
-        <h2 class="fs-md">View more photo's on my Flickr page</h2>
+    <!-- This row only appears on non-mobile devices -->
+    <div class="row justify-content-center my-5 mx-5 no-mobile">
+      <div class="col-12">
         <a target="_blank" class="selectable" href="https://www.flickr.com/photos/197012563@N05/">
           <img class="banner" src="../assets/flickrBG.png" alt="flickr profile image">
         </a>
       </div>
+      <div class="col-12 d-flex justify-content-center">
+        <h2>View more of my photography on my Flickr profile.</h2>
+      </div>
     </div>
+    <!-- This row only appears on mobile devices -->
     <div class="row justify-content-center my-3 mobile-only">
       <h2 class="fs-md">View more photo's on my Flickr page</h2>
       <div class="col-12 d-flex justify-content-center">
@@ -16,9 +20,9 @@
         </a>
       </div>
     </div>
-    <div class="row justify-content-center bg-secondary elevation mt-5">
-      <div class="col-md-6 col-10 pt-3 pb-4">
-        <h2 class="fs-md">Contact me through the store</h2>
+    <div class="row justify-content-center bg-secondary elevation my-5">
+      <div class="col-md-6 col-10 text-center pt-3 pb-4">
+        <h2 class="fs-md pb-3">Or Contact me through my Shopify store</h2>
         <a target="_blank" class="selectable" href="https://getoutsidephotography.shop/pages/contact">
           <img class="banner" src="../assets/Get-Outside-Store-Banner.png" alt="get outside photography store banner">
         </a>
